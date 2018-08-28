5805aa6c3cbfd3fa4f9ac4420e92db0074ff828a
Garga Nous <tristan.presutto@gmail.com>
2018-08-28 20:49:19 +0200

    First commiiiiiiit

---
+++ src/PandaUnificator.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : PandaUnificator.cpp
+// Author      : PandaUnicity
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	cout << "Balec" << endl; // prints Balec
+	return 0;
+}
